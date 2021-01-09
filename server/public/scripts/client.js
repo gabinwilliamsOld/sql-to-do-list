e26eaa86cfc5a5711d3d347a9d5c9a3f3c4e0287
Gabin Williams <gabinwilliams@gmail.com>
2021-01-08 21:40:59 -0600

    added GET ROUTE to get database data

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,13 +3,34 @@ console.log('js');
 $(document).ready(function () {
   console.log('JQ');
  
-  
+  getTasks();
   $('#submitBtn').on('click', addTask);
 
 }); // end doc ready
 
 
 // GET ROUTE
+function getTasks() {
+  $('#taskIn').val('');
+  $('#displayTasks').empty();
+  console.log('in getTasks');
+  // ajax call to server to get tasks
+  $.ajax({
+    type: 'GET',
+    url: '/todo',
+  }).then(function (response) {
+    for (let task of response) {
+
+      $('#displayTasks').append(`
+        <tr class="table-rows" data-id="${task.id}">
+          <td>${task.task}</td>
+          <td><button class="deleteBtn">Delete</button></td>
+        </tr>
+      `);
+    };
+  });
+} // end getTasks
+
 
 // POST ROUTE
 function addTask() {
@@ -21,7 +42,7 @@ function addTask() {
   }
   console.log('Adding task', taskToSend);
 
-  // Send the new koala to the server as data
+  // Send the new task to the server as data
   $.ajax({
     method: 'POST',
     url: '/todo',
@@ -29,8 +50,8 @@ function addTask() {
   })
     .then(function (response) {
       console.log(response);
-      // need get route function here
-      
+      // gets tasks from database
+      getTasks();
     })
     .catch(function (error) {
       console.log('error in task post', error);
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -5,7 +5,22 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 
+// GET
+router.get('/', (req, res) => {
+  let queryText = `
+    SELECT * FROM "todo";
+  `;
 
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('error getting tasks', error);
+      res.sendStatus(500);
+    });
+});
 
 
 // POST
